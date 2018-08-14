cf077a7f52dbd9fe010f6e1f8aa32932a0d8b414
Mhage <masahage@soleil.ocn.ne.jp>
2018-08-14 18:42:30 +0900

    Add files via upload

---
+++ M5Stack_Movie/M5Stack_Movie.ino
@@ -0,0 +1,16 @@
+#include <M5Stack.h>
+
+uint16_t picture[150][200];
+
+void setup() {
+  M5.begin();
+  File file = SD.open("/Movie.dat");
+  while (file.available()) {
+    file.read((uint8_t*)picture, 150 * 200 * 2);
+    M5.Lcd.drawBitmap(60, 45, 200, 150, (uint8_t*)picture);
+    delay(20);
+  }
+}
+
+void loop() {
+}
